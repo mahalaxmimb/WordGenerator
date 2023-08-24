eaf9791c2d937eda7d140dde2ed1aca819bf7ae6
mahalaxmimb <44541320+mahalaxmimb@users.noreply.github.com>
2023-08-24 12:25:45 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import arrayWords from '../utils/words';
+import countryWords from '../utils/countries';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+
+  words = '';
+  limit = 10;
+  countries = '';
+
+  handleSlideChange(newLimit: number){
+    this.limit = newLimit;
+  }
+  randomnum = 10;
+  generateCountries(){
+    this.randomnum= Math.floor(Math.random() * 196) + 1;
+    this.limit = this.randomnum;
+    this.countries = countryWords.slice(0, this.randomnum).join(' ');
+  }
+  generate(){
+    this.words = arrayWords.slice(0, this.limit).join(' ');
+  }
 }
